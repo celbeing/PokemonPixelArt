77f86c0a331a316a2761b480e2f9c6ded12882f4
celbeing <kimsd1983@naver.com>
2022-05-13 22:09:08 +0900

    22.05.13
    포켓몬 데이터 csv

---
+++ pokemon/pokemon_dictionary/pokemon_dictionary/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace pokemon_dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file_path = 
+                @"C:\\Users\\kimsd\\source\\repos\\Csharp_Study\\pokemon";
+            StreamReader file_stream = 
+                new StreamReader(file_path + "\\pokemon_global_name.txt");
+            List<string> file_read = new List<string>();
+            List<string[]> pokemon_dictionary = new List<string[]>();
+            string readline;
+            while((readline = file_stream.ReadLine()) != null)
+            {
+                if (readline == "-") continue;
+                else
+                {
+                    string[] current = readline.Split(',');
+                    string[] pokemon = { current[0], current[1], current[3] };
+                    pokemon_dictionary.Add(pokemon);
+                }
+            }
+            using (StreamWriter file = 
+                new StreamWriter(file_path + "\\pokemon_translate.csv", 
+                false, Encoding.GetEncoding("utf-8")))
+            {
+                file.WriteLine("영어,한글");
+                foreach (string[] pokemon in pokemon_dictionary)
+                    file.WriteLine($"{pokemon[2].ToLower()},{pokemon[1]}");
+            }
+        }
+    }
+}
